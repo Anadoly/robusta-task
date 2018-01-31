c8352d058a60b5e4b06c5412f4fc8aa35570bcf8
anadoly <ahmedelanadoly@gmail.com>
2018-01-31 16:12:14 +0200

    Next and previous button

--- src/components/Todos/index.js
+++ src/components/Todos/index.js
@@ -17,8 +17,13 @@ export default class Todos extends Component {
 			newTodo: '',
 			todos: [],
 			loadingTodos: false,
-			visible: false
-		}
+			visible: false,
+			currentPage: 1,
+      		todosPerPage: 5
+		};
+		// this.handleClick = this.handleClick.bind(this);
+		this.handleNextClick = this.handleNextClick.bind(this);
+		this.handlePreviousClick = this.handlePreviousClick.bind(this);
 	}
 	componentWillMount(){
         this._fetchTodos();
@@ -27,12 +32,23 @@ export default class Todos extends Component {
     this.setState({
       visible: true
     });
-  }
+ 	}
+
    	handleCancel = (e) => {
 	    this.setState({
 	      visible: false
 	    });
 	}
+	handleNextClick() {
+	    this.setState({
+	      currentPage: this.state.currentPage + 1
+	    });
+  	}
+  	handlePreviousClick() {
+	    this.setState({
+	      currentPage: this.state.currentPage - 1
+	    });
+  	}
 	_handleChange(id){
 		var todos = this.state.todos.slice(0);
 		var done;
@@ -102,6 +118,19 @@ export default class Todos extends Component {
 	render()
 	{
 		const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
+		const { todos, currentPage, todosPerPage } = this.state;
+		const indexOfLastTodo = currentPage * todosPerPage;
+	    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+	    const currentTodos = todos.slice(indexOfFirstTodo, indexOfLastTodo);
+	    const renderTodos = currentTodos.map((todo) => {
+      		 return  (
+                   <Todo todo={todo} handleChange ={this._handleChange.bind(this)} delete = {this._deleteTodo.bind(this, todo)} />
+					)
+    	});
+    	const pageNumbers = [];
+    		for (let i = 1; i <= Math.ceil(todos.length / todosPerPage); i++) {
+      		pageNumbers.push(i);
+    		}   	
 		return (
             <div >	
             	<div className="Todos">
@@ -133,17 +162,21 @@ export default class Todos extends Component {
                     this.state.loadingTodos
                     ? (<div><Spin indicator={antIcon} /></div>)
 
-                    : this.state.todos.map((todo) => {
-                        return  (
-                            		<Todo todo={todo} handleChange ={this._handleChange.bind(this)} delete = {this._deleteTodo.bind(this, todo)} />
-							 	)
-                    	})
+                    : 	<div>
+	          					{renderTodos}
+					        
+					     </div>
                 }
                  <div className="border1">
                         <div className="border2">
                 <div className="button-div">
-                            <Button><span className="span"><Icon type="play-circle" /></span>previous</Button>
-                            <Button>Next<Icon type="play-circle" /></Button>
+                			{currentPage === 1
+                				?<Button onClick={this.handlePreviousClick} disabled><span className="span"><Icon type="play-circle" /></span>previous</Button>
+                				:<Button onClick={this.handlePreviousClick}><span className="span"><Icon type="play-circle" /></span>previous</Button>
+                			}
+                			{currentPage === (pageNumbers.length) 
+                            ? <Button onClick={this.handleNextClick} disabled>Next<Icon type="play-circle"  /></Button>
+                    		: <Button onClick={this.handleNextClick}>Next<Icon type="play-circle"  /></Button>}
                         </div>
                     </div>
                     </div>
